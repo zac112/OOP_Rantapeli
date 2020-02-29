05986c2b084b461e267b25305936458bce4bf70a
Milla Sipilä <51693537+Mill4@users.noreply.github.com>
2020-02-29 14:37:29 +0200

    XP bar script

---
+++ Assets/Maski.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maski : MonoBehaviour
+{
+[SerializeField]
+GameObject mask = null;
+
+public void MoveItem(int prosent)
+{
+    float move = 5*(prosent/100);
+    float x= mask.transform.position.x;
+    float y = mask.transform.position.y;
+    float z = mask.transform.position.z; 
+    mask.transform.position = new UnityEngine.Vector3(x-move,y,z);
+}
+
+void Update()
+{
+    MoveItem(50);
+}
+
+}
