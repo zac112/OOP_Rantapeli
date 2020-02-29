d12ea93c9a2cb8c32ed55a6fa4f1fd880384863a
Juhana Kuparinen <jmkupa@utu.fi>
2020-02-29 16:30:00 +0200

    Branch thirst luotu.

---
+++ Assets/scripts/thirstlevel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thirstlevel : MonoBehaviour
+{
+    public int thirst = 5;
+    // Update is called once per frame
+    void Start()
+    {
+        IEnumerator coroutine = ThirstGrow();
+        StartCoroutine(coroutine);
+    }
+    private IEnumerator ThirstGrow()
+    {
+        while (true)
+        {
+            float waitTime = 4f;
+
+            yield return new WaitForSeconds(waitTime);
+            Debug.Log(thirst);
+            thirst--;
+            if (thirst == 0)
+            {
+                Object.Destroy(gameObject);
+            }
+        }
+    }
+}
