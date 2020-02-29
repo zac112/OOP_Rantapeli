56401348ef19cfca51d0fd63fe1fc27aab08c476
Antti <cursedtomb@LAPTOP-L7NICNE2.localdomain>
2020-02-29 16:41:03 +0200

    health

---
+++ Assets/scripts/ShowHP.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowHP : MonoBehaviour
+{
+
+    public int HP;
+
+    void Update()
+    {
+        if (HP == 3)
+        {
+            gameObject.GetComponent<Image>().sprite = Resources.Load<Sprite>("3HP");
+        }
+        else if(HP == 2)
+        {
+            gameObject.GetComponent<Image>().sprite = Resources.Load<Sprite>("2HP");
+        }
+        else if (HP == 1)
+        {
+            gameObject.GetComponent<Image>().sprite = Resources.Load<Sprite>("1HP");
+        }
+        else
+        {
+            gameObject.GetComponent<Image>().sprite = Resources.Load<Sprite>("0HP");
+        }
+    }
+}
