c1bde81787dce6f0ac8e36372d6b47234c191256
Rasmus Jokinen <raajok@utu.fi>
2020-02-29 12:44:47 +0200

    liikkumisscript

---
+++ Assets/LikkuminenSivuille.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LikkuminenSivuille : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 position = this.transform.position;
+            position.x = position.x - 0.1f;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 position = this.transform.position;
+            position.x = position.x + 0.1f;
+            this.transform.position = position;
+        }
+    }
+}
--- Assets/scripts/movement.cs
+++ Assets/scripts/movement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movement : MonoBehaviour
+public class Movement : MonoBehaviour
 {
     public float speed;
     public float jumpForce;
