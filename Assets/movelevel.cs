d994a552ad86d2e123f55f9033a4ddc034fb7d65
Teemu Salonen <tpsalo@utu.fi>
2020-02-29 11:54:08 +0200

    Vaihdeltu vähän arvoja

--- Assets/movelevel.cs
+++ Assets/movelevel.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class movelevel : MonoBehaviour
 {
+    public float speed = 250f;
     // Start is called before the first frame update
     void Start()
     {
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        rb.AddForce(Vector3.left*200f);
+        rb.AddForce(Vector3.left*speed);
     }
 
     // Update is called once per frame
