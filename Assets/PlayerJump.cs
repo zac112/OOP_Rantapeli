d1a59a12f2f72434250ae7cb08144557bbb9ee44
Nevil Sandaradura <npsand@utu.fi>
2020-02-29 14:32:01 +0200

    hyppy fixattu

---
+++ Assets/PlayerJump.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    bool isGrounded;
+    public float jumpHeight;
+    Vector3 jump;
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isGrounded = false;
+    }
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(jump * jumpHeight, ForceMode2D.Impulse);
+            //isGrounded = false;
+        }
+    }
+}
--- Assets/movelevel.cs
+++ Assets/movelevel.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class movelevel : MonoBehaviour
 {
-    public float speed = 250f;
+    public float speed = 5;
 
     // Start is called before the first frame update
     void Start()
