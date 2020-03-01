25e3baf602b8aaa1d30cdfca3701aa517ccb5934
Evert Mäkinen <eomaki@utu.fi>
2020-03-01 17:19:32 +0200

    epic

---
+++ Assets/cameraMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+    public Transform player;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.position.x+2 > transform.position.x)
+            transform.position = new Vector3(player.position.x+2, transform.position.y, transform.position.z);        
+    }
+}
