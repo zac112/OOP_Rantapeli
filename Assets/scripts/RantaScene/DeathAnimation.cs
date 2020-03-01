254db1f7e08caa59be4e91a7cb93187bbcc415b5
harkonenhm <hemhar@utu.fi>
2020-03-01 11:13:19 +0200

    death animation

---
+++ Assets/scripts/RantaScene/DeathAnimation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 0.005f, gameObject.transform.position.z);
+    }
+}
