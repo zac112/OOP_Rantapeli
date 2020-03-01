849690c042471e1d9b8389d8b823cfc3ac07a7f8
Rasmus Jokinen <raajok@utu.fi>
2020-03-01 12:52:25 +0200

    voitto ajan kanssa

---
+++ Assets/scripts/siderunner/Win.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Win : MonoBehaviour
+{
+    [SerializeField]
+    public int runinKesto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("OdotaVoittoa");
+    }
+
+    IEnumerator OdotaVoittoa()
+    {
+        yield return new WaitForSeconds(runinKesto);
+        GameState.WinRun();
+    }
+}
