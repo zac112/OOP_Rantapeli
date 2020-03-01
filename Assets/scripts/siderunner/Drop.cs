0ced8a968ef1a8e9374d8961e2ad7df9078e8fe0
Rasmus Jokinen <raajok@utu.fi>
2020-03-01 12:36:50 +0200

    Health

--- Assets/scripts/siderunner/Drop.cs
+++ Assets/scripts/siderunner/Drop.cs
@@ -18,7 +18,6 @@ public class Drop : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("Toimii!");
         Object.Destroy(gameObject);
     }
 }
---
+++ Assets/scripts/siderunner/Health.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Pile"))
+        {
+            health--;
+        }
+            
+        if (health < 1)
+        {
+            GameState.LoseRun();
+        }
+    }
+}
