6296a240363149b479c78d18dd7d8c0f8520879e
Jooakar <59924872+Jooakar@users.noreply.github.com>
2020-03-01 17:22:42 +0200

    joo

---
+++ Assets/scripts/BigChungus/AnimaatioController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimaatioController : MonoBehaviour
+{
+    int s;
+    Animator anim;
+    health h;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        h = gameObject.GetComponent<health>();
+        s = h.healthPoints;
+    }
+
+    void Update()
+    {
+        if(h.healthPoints < s)
+        {
+            s = h.healthPoints;
+            anim.Play("TakeDMG");
+        }
+    }
+}
