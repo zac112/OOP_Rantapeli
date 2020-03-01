0ed2ebea37d9443f585f1f3a9f640bf6e4772e4c
Juhana Kuparinen <jmkupa@utu.fi>
2020-03-01 16:31:58 +0200

    Fixattu bugeja.

--- Assets/scripts/RantaScene/Clicking.cs
+++ Assets/scripts/RantaScene/Clicking.cs
@@ -6,13 +6,18 @@ using UnityEngine.SceneManagement;
 public class Clicking : MonoBehaviour
 {
     [SerializeField] int[] scenes;
+    public static bool clickAllowed = true;
     private void OnMouseDown()
     {
-        GameState.SetMaali(gameObject);
-        DontDestroyOnLoad(gameObject);        
-        Destroy(this);
-        Destroy(GetComponent<Dying>());
-        GameObject.FindGameObjectWithTag("Player").GetComponent<CutSceneTimer>().GoSaveVictims();
+        if (clickAllowed)
+        {
+            GameState.SetMaali(gameObject);
+            DontDestroyOnLoad(gameObject);        
+            Destroy(this);
+            Destroy(GetComponent<Dying>());
+            GameObject.FindGameObjectWithTag("Player").GetComponent<CutSceneTimer>().GoSaveVictims();
+
+        }
     }
 
 }
