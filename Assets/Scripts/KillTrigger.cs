0aed39b6c2afc15118eeafdbddd26024156dcf19
Teemu Salonen <tpsalo@utu.fi>
2020-03-01 11:30:47 +0200

    lisätty killtrigger

---
+++ Assets/Scripts/KillTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillTrigger : MonoBehaviour
+{
+    void Start()
+    {
+        Collider2D player = GetComponent<Collider2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D player){
+        if (player.tag == "Player"){
+            player.transform.localPosition = new Vector3(-9.949f, -1.691f, 0.0f);
+            Debug.Log("asd");
+         }
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
