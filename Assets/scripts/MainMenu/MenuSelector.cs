20e4189bdb01095dac326124923c35e025595dcc
Juhana Kuparinen <jmkupa@utu.fi>
2020-03-01 12:42:18 +0200

    Main menu valmis. Pyörivä liike lisätty nuolelle.

--- Assets/scripts/MainMenu/MenuSelector.cs
+++ Assets/scripts/MainMenu/MenuSelector.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuSelector : MonoBehaviour
 {
+    public float xAngle, yAngle;
     [SerializeField]
     GameObject[] points;
     int i = 0;
@@ -18,6 +20,10 @@ public class MenuSelector : MonoBehaviour
         Debug.Log("Heey");
         while (true)
         {
+            //Nuolen pyöritys
+            gameObject.transform.Rotate(new Vector3(1.0f, 0.0f, 0.0f), Space.Self);
+
+            //Pelaajan valintojen managerointi
             if (Input.GetKeyDown("s") || Input.GetKeyDown("down"))
             {
                 i = Mathf.Clamp(i+1, 0, points.Length-1);
@@ -29,6 +35,22 @@ public class MenuSelector : MonoBehaviour
                 i = Mathf.Clamp(i - 1, 0, points.Length-1);
                 Debug.Log(i);
                 gameObject.GetComponent<Transform>().position = points[i].GetComponent<Transform>().position;
+            }
+            //Jos pelaaja painaa välilyöntiä, ladataan haluttu scene
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (i == 0)
+                {
+                    SceneManager.LoadScene(0);
+                }
+                if (i == 1)
+                {
+                    SceneManager.LoadScene(0);
+                }
+                if (i == 2)
+                {
+                    Application.Quit(0);
+                }
             }
             yield return null;
         }
